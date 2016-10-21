4fb353757a2372809a59461aa063c275075a6898
Emanuel\Emanuel <isw.emanuel.campos.jasso@gmail.com>
2016-10-21 11:19:20 -0500

    Validaciones usuarios

--- application/views/administrador/vusuario.php
+++ application/views/administrador/vusuario.php
@@ -1,36 +1,22 @@
-<<<<<<< HEAD
-		<div class="row">
-			<!--TABLA DE VISUALIZACOIN DE DATOS-->
-			<div class="container text-center" >
-		 		<div class="cols-xs-5">
-		         	<h1 align="left">Usuarios</h1>        
-		         	<div class="col-xs-13" align="right">
-		         		<br>
-		         		<label>Nombre:</label>
-		         		<input type="text" name="txtBuscarU" id="txtBuscarU">          		         			
-		    	     	<a class="btn btn-default" href="#" role="button" data-toggle="modal" data-target="#myModal"> Agregar <span class="glyphicon glyphicon-plus"></span></a>		    	    	 
-		    		</div>
-		    		<br>
-					<div class="cols-xs-13">					
-						<div class="row" >
-							<table class="table table-bordered" id="tablaDatosU">
-								<thead>
-									<tr>
-										<td class="text-center">Nombre</td>
-										<td class="text-center">Correo electrónico</td>
-										<td class="text-center">Teléfono</td>
-										<td id="tel" class="text-center">Area asignada</td>
-										<td class="text-center">Editar</td>
-									</tr>
-								</thead>							
-								<tbody>	
-								</tbody>										
-							</table>					
-						</div>
-					</div>				
-				</div>
-			</div>
-=======
+<!-- <script type="text/javascript" src= "jquery-1.7.1.min..js"></script> -->
+<style type="text/css">
+	.errores{
+		/*-webkit-box-shadow: 0 0 10px rgba(0,0,0,0.3);
+		-moz-box-shadow: 0 0 10px rgba(0,0,0,0.3);
+		-o-box-shadow: 0 0 10px rgba(0,0,0,0.3);*/
+		background: red;
+		box-shadow: 0 0 10px rgba(0,0,0,0.3);
+		color: #fff;
+		display: none;
+		font-size: 10px;
+		margin-top: -10px;
+		margin-left: 150px;
+		padding: 5px;
+		width: 145px;
+		height: 30px;
+		position: absolute;
+	}
+</style>
 <div class="row">
   <div class="container text-center">
     <div class="row">
@@ -40,27 +26,19 @@
         <div class="col-xs-12 col-md-8" align="right">
           <br>
           <label>Nombre: </label>
-          <input type="text" name="txtBuscarU" id="txtBuscarU">
-           <a class="btn btn-default" href="#" role="button" data-toggle="modal" data-target="" id="btnBuscarA"><span class="glyphicon glyphicon-search"></span></a>
-          <a class="btn btn-default" href="#" role="button" data-toggle="modal" data-target="#myModal"> Agregar <span class="glyphicon glyphicon-plus"></span></a>
+          <input type="text" name="txtBuscarU" id="txtBuscarU">           
+          <a class="btn btn-default" href="#" role="button" data-toggle="modal" data-target="#myModal" id="btnAgregarU" name="btnAgregarU"> Agregar <span class="glyphicon glyphicon-plus"></span></a>
         </div>
     </div>
     <br>
   </div>
 </div> 
->>>>>>> origin/master
 <!--FINDE TABLA DE VISUALIZACION DE DATOS-->
 <div class="container">
   <div class="row container" >
     <table class="table table-bordered" id="tablaDatosU">
       <thead>
-        <!-- <tr>
-          	<td class="text-center">Nombre</td>
-			<td class="text-center">Correo electrónico</td>
-			<td class="text-center">Teléfono</td>
-			<td id="tel" class="text-center">Area asignada</td>
-			<td class="text-center">Opción</td>
-        </tr> -->
+
       </thead>              
     </table>          
   </div>
@@ -78,57 +56,60 @@
 	      		</div>
 	    		<div class="modal-body">
 	      		<!--Formulario de registro-->	
-					<form name ="usuario" id="datos_usuario" method="POST" action="<?php echo base_url()?>cusuario/validar"> 					
+					<!-- <form name ="usuario" id="datos_usuario" method="POST" action="<?php// echo base_url()?>cusuario"> 					 -->
+					<form name ="usuario" id="datos_usuario" method="POST" > 					
 						<div class="row">
 							<div class="col-xs-6"x>
 								<div class="form-group" align="left">
-									<label>Nombre:</label> 									 
-									<input type="text" class="form-control" id="nombre_usuario" name="txtnombre" >
+									<label>Nombre:</label> 											
+									<input type="text" class="form-control" id="txtnombre" name="txtnombre">					
 									<label>Apellido paterno:</label> 
-									<input type="text" class="form-control" id="apellido_paterno" name="txtap">
+									<input type="text" class="form-control" id="txtap" name="txtap">	
 									<label>Apellido materno:</label> 
-									<input type="text" class="form-control" id="apellido_materno" name="txtam">
+									<input type="text" class="form-control" id="txtam" name="txtam">								
 									<label>Sexo:</label>
 									Hombre <input type="radio" name="sexo" value="H" id="sexo_hombre" >
 									Mujer <input type="radio" name="sexo" value="M" id="sexo_mujer"><br>
+									<div class="errores" id="esexo">Seleeciona sexo de usuario</div>
 									<label>Matricula:</label> 
-									<input type="text" class="form-control" id="matricula_usuario" name="txtmatricula">
+									<input type="text" class="form-control" id="txtmatricula" name="txtmatricula">
 									<label>Correo electrónico:</label> 
-									<input type="text" class="form-control" id="correo_usuario" name="txtcorreo">
+									<input type="text" class="form-control" id="txtcorreo" name="txtcorreo">
 									<label>Institución de procedencia:</label>
-									<input type="text" name="txtinstitucion" class="form-control" id="institucion_usuario">
+									<input type="text" name="txtinstitucion" class="form-control" id="txtinstitucion">
 									<label>Semestre:</label> 
-									<input type="number" class="form-control" id="semestre_usuario" name="txtsemestre">
+									<input maxlength="2" minlength="1" class="form-control" id="txtsemestre" name="txtsemestre">
 								</div>
 							</div>
 							<div class="col-xs-6">
 								<div class="form-group" align="left">
 									<label>Teléfono:</label> 
-									<input type="number" class="form-control" id="telefono_usuario" name="txttelefono">
+									<input class="form-control" id="txttelefono" name="txttelefono" type="text">
 									<label>Fecha de alta:</label>
-									<input type="date"  class="form-control" id="fecha_alta" name="txtalta">
+									<input type="date"  class="form-control" id="txtalta" name="txtalta">
+									<div class="errores" id="ealta">Ingrese fecha de alta</div>
 									<label>Fecha de inicio:</label>
-									<input type="date"  class="form-control" id="fecha_inicio" name="txtinicio">
+									<input type="date"  class="form-control" id="txtinicio" name="txtinicio">
 									<label>fecha de término:</label>
-									<input type="date" class="form-control"  id="fecha_fin" name="txtfin">
+									<input type="date" class="form-control"  id="txtfin" name="txtfin">
 									<label>Tipo de usuario:</label><br>
-									<select name="ctipousuario">										
+									<select name="ctipousuario" id="cbotipousuario">										
 										<option value="1">Servicio social</option>
 										<option value="0">Administrador</option>
 									</select><br>
 									<label>Nombre de usuario:</label>
-									<input type="text" class="form-control" name="txtuser" id="user_usuario">
+									<input type="text" class="form-control" name="txtuser" id="txtuser">
 									<label>Contraseña:</label><br>
-									<input type="password" name="txtpassword" id="pass_usuario"><br>
+									<input type="password" name="txtpassword" id="txtpassword"><br>
 									<label>Area:</label><br>
-									<select id="cboArea" name="cboArea">										
+									<select id="cboArea" name="cboArea">									
 									</select>									
 								</div>
 							</div>
 						</div>
 						<?php if (validation_errors()): ?><div><label class="label-warning "><?php echo validation_errors(); ?></label></div><?php endif; ?> 
 						<div class="modal-footer">	        	
-	        				<input type="submit" value="Guardar" name="">
+	        				<input type="submit" value="Guardar" name="btnGuardarU" id="btnGuardarU">
 	      				</div>
 					</form>
 				</div>
@@ -200,7 +181,7 @@
 					</div>
 				</div>
 				<div class="modal-footer">	        	
-       				<input type="submit" value="Guardar" name="">
+       				<input type="submit" value="Guardar" name="btnEditarU" id="btnEditarU">
 				</div>
 				</form>
 			</div>	
--- js/jusuarios.js
+++ js/jusuarios.js
@@ -1,119 +1,313 @@
-	
-//$('#myModal').modal({ backdrop: 'static', keyboard: false })
 $(document).on("ready", inicioUsuarios)
-function inicioUsuarios(){
-	mostrarDatosU("");
-	$("#txtBuscarU").keyup(function(){
-		buscarU =$ ("#txtBuscarU").val();	
-		mostrarDatosU(buscarU);
-	});
-	$("form").submit(function(event){
-		//event.preventDefault();
-	});
+function inicioUsuarios() {
+    validarTipeoCajas();
+    mostrarDatosU("");
+    $("#txtBuscarU").keyup(function () {
+        buscarU = $("#txtBuscarU").val();
+        mostrarDatosU(buscarU);
+    });
+    $("form").submit(function (event) {
+        //event.preventDefault();
+    });
+    $("body").on("click", "#tablaDatosU button", function (event) {
+        idsele = $(this).attr("value");
+        $.post(baseurla + "cusuario/mostrarUsuarioSolo", {
+        idsele
+        },
+                function(data){
+                var r = JSON.parse(data);
+                $.each(r, function (i, item) {
+                    id = item.idUsuario;
+                    nombre = item.nombre;
+                    paterno = item.apellidoPaterno;
+                    materno = item.apellidoMaterno;
+                    sexo = item.sexo;
+                    matricula = item.matricula;
+                    correo = item.correo;
+                    procedencia = item.institucionProcedencia;
+                    semestre = item.semestre;
+                    telefono = item.telefono;
+                    alta = item.fechaAlta;
+                    inicio = item.fechaInicio;
+                    termino = item.fechaTermino;
+                    tipo = item.tipoUsuario;
+                    user = item.user;
+                    pass = item.password;
+                    inicial = item.passInicial;
+                    area = item.idArea;
+                });
+                switch (sexo) {
+                    case 'H':
+                        document.getElementById('sexoseleh').checked = true;
+                        break;
+                    case 'M':
+                        document.getElementById('sexoselem').checked = true;
+                }
+                $('#id').val(id);
+                $('#txtnombresele').val(nombre);
+                $('#txtapsele').val(paterno);
+                $('#txtamsele').val(materno);
+                $('#sexosele').val(sexo);
+                $('#txtmatriculasele').val(matricula);
+                $('#txtcorreosele').val(correo);
+                $('#txtinstitucionsele').val(procedencia);
+                $('#txtsemestresele').val(semestre);
+                $('#txttelefonosele').val(telefono);
+                $('#txtaltasele').val(alta);
+                $('#txtiniciosele').val(inicio);
+                $('#txtfinsele').val(termino);
+                $('#ctipousuariosele').val(tipo);
+                $('#txtusersele').val(user);
+                $('#txtpasswordsele').val(pass);
+                $('#cboAreasele').val(area);
+            });
+    });
+            $("body").on("click", "#tablaDatosU a", function (event) {
+        event.preventDefault();
+        idsele = $(this).attr("href");
+        eliminar(idsele);
+    });
+    $("#btnGuardarU").click(function () {        
+        if ($("#txtnombre").val() === "") {	// Validacoin de la caja de texto nombre de usuario
+            document.getElementById("txtnombre").style.borderColor = "red";
+            $("#txtnombre").attr("placeholder", "Verifique nombre de usuario");
+            $("#txtnombre").focus();
+            return false;
+        } else {
+            document.getElementById("txtnombre").style.borderColor = "";
+            if ($("#txtap").val() === "") { // Validcion de caja de texto apellido paterno
+                document.getElementById("txtap").style.borderColor = "red";
+                $("#txtap").attr("placeholder", "Verifique apellido paterno");
+                $("#txtap").focus();
+                return false;
+            } else {
+                document.getElementById("txtap").style.borderColor = "";
+                if ($("#txtam").val() === "") { // Validaciion de apellido materno
+                    document.getElementById("txtam").style.borderColor = "red";
+                    $("#txtam").attr("placeholder", "Verifique apellido materno");
+                    $("#txtam").focus();
+                    return false;
+                } else {
+                    document.getElementById("txtam").style.borderColor = ""; //validacion de checkbox de sexo
+                    if (!document.getElementById("sexo_hombre").checked && !document.getElementById("sexo_mujer").checked) {
+                        $("#esexo").fadeIn();
+                        $("#sexo_hombre").focus();
+                        $("#sexo_mujer").focus();
+                        return false;
+                    } else {
+                        $("#esexo").fadeOut();
+                        if($("#txtmatricula").val() === ""){ //Validacion de matricula
+                            document.getElementById("txtmatricula").style.borderColor = "red";
+                            $("#txtmatricula").attr("placeholder", "Verifique matricula");
+                            $("#txtmatricula").focus();
+                            return false;
+                        }else{
+                            document.getElementById("txtmatricula").style.borderColor ="";
+                            //var exp = new RegExp ("/[^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$]/");                            
+                            if($("#txtcorreo").val() === ""){ 
+                                document.getElementById("txtcorreo").style.borderColor = "red";
+                                $("#txtcorreo").attr("placeholder", "Verifique correo");
+                                $("#txtcorreo").focus();
+                                return false;
+                            }else{
+                                document.getElementById("txtcorreo").style.borderColor = "";
+                                if($("#txtinstitucion").val() == ""){
+                                    document.getElementById("txtinstitucion").style.borderColor= "red";
+                                    $("#txtinstitucion").attr("placeholder", "Verifique instituci�n de procedecia");
+                                    $("#txtinstitucion").focus();
+                                    return false;
+                                }else{
+                                    document.getElementById("txtinstitucion").style.borderColor = "";
+                                    if($("#txtsemestre").val() === "" || $("#txtsemestre").val() <= 0 || $("#txtsemestre").val() > 10){
+                                        $("#txtsemestre").val("");
+                                        document.getElementById("txtsemestre").style.borderColor = "red";
+                                        $("#txtsemestre").attr("placeholder", "Verifique semestre (1� - 10�)");
+                                        $("#txtsemestre").focus();
+                                        return false;
+                                    }else{
+                                        document.getElementById("txtsemestre").style.borderColor = "";
+                                        var tel = $("#txttelefono").val();
+                                        if($("#txttelefono").val() ==="" || $("#txttelefono").val().length !== 10){                                        
+                                            document.getElementById("txttelefono").style.borderColor = "red";
+                                            $("#txttelefono").val("");
+                                            $("#txttelefono").attr("placeholder", "Verifique telefono (10 digitos)");
+                                            $("#txttelefono").focus();
+                                            return false;
+                                        }else{
+                                            document.getElementById("txttelefono").style.borderColor = "";
+                                            if($("#txtalta").val() ===""){
+                                                document.getElementById("txtalta").style.borderColor = "red";
+                                                $("#ealta").fadeIn();
+                                                $("#txtalta").focus();
+                                                return false;
+                                            }else{
+                                                $("#ealta").fadeOut();
+                                                document.getElementById("txtalta").style.borderColor ="";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-	$("body").on("click", "#tablaDatosU button", function(event){
-		idsele=$(this).attr("value");				
-		$.post(baseurla+"cusuario/mostrarUsuarioSolo",{
-			idsele
-		},
-		function(data){			 
-			var r = JSON.parse(data);
-			$.each(r, function(i,item){
-				id = item.idUsuario;
-				nombre = item.nombre;
-				paterno = item.apellidoPaterno;
-				materno = item.apellidoMaterno;
-		 		sexo = item.sexo;
-		 		matricula = item.matricula;
-		 		correo = item.correo;
-		 		procedencia = item.institucionProcedencia;
-		 		semestre = item.semestre;
-		 		telefono = item.telefono;
-		 		alta = item.fechaAlta;
-		 		inicio = item.fechaInicio;
-		 		termino = item.fechaTermino;
-		 		tipo = item.tipoUsuario;
-		 		user = item.user;
-		 		pass = item.password;
-		 		inicial = item.passInicial;
-		 		area = item.idArea;
-			});			
-			switch(sexo){
-				case 'H': document.getElementById('sexoseleh').checked = true;
-					break;
-				case 'M': document.getElementById('sexoselem').checked = true;
-			}
-			$('#id').val(id);
-			$('#txtnombresele').val(nombre);
-			$('#txtapsele').val(paterno);
-			$('#txtamsele').val(materno);
-			$('#sexosele').val(sexo);
-			$('#txtmatriculasele').val(matricula);
-			$('#txtcorreosele').val(correo);
-			$('#txtinstitucionsele').val(procedencia);
-			$('#txtsemestresele').val(semestre);
-			$('#txttelefonosele').val(telefono);
-			$('#txtaltasele').val(alta);
-			$('#txtiniciosele').val(inicio);
-			$('#txtfinsele').val(termino);
-			$('#ctipousuariosele').val(tipo);
-			$('#txtusersele').val(user);
-			$('#txtpasswordsele').val(pass);
-			$('#cboAreasele').val(area);
-		});
 
-	});
-	$("body").on("click","#tablaDatosU a",function(event){
-		event.preventDefault();
-		idsele=$(this).attr("href");		
-		eliminar(idsele);
-	});
+    });
 }
 
 function mostrarDatosU(valor)
 {
-	$.ajax({		
-		url:baseurla+"cusuario/mostrarUsuario",
-		type:"POST",
-		data:{buscarU:valor},
-		success:function(respuesta){			
-			var registros=eval(respuesta);
-			$('#tablaDatosU').html(
-		'<tr>'+
-			'<td class="text-center">Nombre</td>'+
-			'<td class="text-center">Correo electrónico</td>'+
-			'<td class="text-center">Teléfono</td>'+
-			'<td id="tel" class="text-center">Area asignada</td>'+
-			'<td class="text-center">Editar</td>'+
-		'</tr>');
-			$.each(registros, function(i,item){
-				$('#tablaDatosU').append(
-						'<tr>'+
-						'<td align ="center">'+item.nombre+' '+item.apellidoPaterno+' '+item.apellidoMaterno+'</td>'+
-						'<td align ="center">'+item.correo+'</td>'+
-						'<td align ="center">'+item.telefono+'</td>'+
-						'<td align ="center">'+item.nombreArea+'</td>'+												
-						'<td height=5 width=120><button type="button" class="btn btn-primary" role="button" data-toggle="modal" data-target="#editar" value="'+item.idUsuario+'" id="btnEditar"> <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>  </button>'+ ' '+ 
-						'<a class="btn btn-danger btn-ms" href="'+item.idUsuario+'" > <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>'+
-						'</tr>'		
-					);
-			});
-		}
-	});
+    $.ajax({
+        url: baseurla + "cusuario/mostrarUsuario",
+        type: "POST",
+        data: {buscarU: valor},
+        success: function (respuesta) {
+            var registros = eval(respuesta);
+            $('#tablaDatosU').html(
+                    '<tr>' +
+                    '<td class="text-center">Nombre</td>' +
+                    '<td class="text-center">Correo electrónico</td>' +
+                    '<td class="text-center">Teléfono</td>' +
+                    '<td id="tel" class="text-center">Area asignada</td>' +
+                    '<td class="text-center">Editar</td>' +
+                    '</tr>');
+            $.each(registros, function (i, item) {
+                $('#tablaDatosU').append(
+                        '<tr>' +
+                        '<td align ="center">' + item.nombre + ' ' + item.apellidoPaterno + ' ' + item.apellidoMaterno + '</td>' +
+                        '<td align ="center">' + item.correo + '</td>' +
+                        '<td align ="center">' + item.telefono + '</td>' +
+                        '<td align ="center">' + item.nombreArea + '</td>' +
+                        '<td height=5 width=120><button type="button" class="btn btn-primary" role="button" data-toggle="modal" data-target="#editar" value="' + item.idUsuario + '" id="btnEditar"> <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>  </button>' + ' ' +
+                        '<a class="btn btn-danger btn-ms" href="' + item.idUsuario + '" > <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>' +
+                        '</tr>'
+                        );
+            });
+        }
+    });
 }
 
 function eliminar(idsele)
-{	
-	$.ajax({
-		url:baseurla+"cusuario/eliminar",
-		type:"POST",
-		data:{id:idsele},
-		success:function(respuesta){
-			alert(respuesta);
-			mostrarDatosU("");
-		}
+{
+    $.ajax({
+        url: baseurla + "cusuario/eliminar",
+        type: "POST",
+        data: {id: idsele},
+        success: function (respuesta) {
+            alert(respuesta);
+            mostrarDatosU("");
+        }
 
-	});
+    });
+}
+
+function guardar() {
+    var nombre = $("#txtnombre").val();
+    var app = $("#txtap").val();
+    var apm = $("#txtam").val();
+    var sex = '';
+    if (document.getElementById("sexo_hombre").checked) {
+        sex = "H";
+    } else {
+        sex = "M";
+    }
+    var mat = $("#txtmatricula").val();
+    var core = $("#txtcorreo").val();
+    var ins = $("#txtinstitucion").val();
+    var sem = $("#txtsemestre").val();
+    var tel = $("#txttelefono").val();
+    var alta = $("#txtalta").val();
+    var ini = $("#txtinicio").val();
+    var ter = $("#txtfin").val();
+    var tu = $("#cbotipousuario").val();
+    var user = $("#txtuser").val();
+    var pass = $("#txtpassword").val();
+    var area = $("#cboArea").val();
+    alert(nombre + " " + app + " " + apm + " " + sex + " " + mat + " " + core + " " + ins + " " + sem + " " + tel + " " + alta + " " + ini + " " + ter + " " + tu + " " + user + " " + pass + " " +
+            area);
+    $.ajax({
+        type: "POST",
+        url: baseurla + "cusuario/guardar/",
+        data: {
+            'nombre': nombre,
+            'apellidoPaterno': app,
+            'apellidoMaterno': apm,
+            'sexo': sex,
+            'matricula': mat,
+            'correo': core,
+            'institucion': ins,
+            'semestre': sem,
+            'telefono': tel,
+            'fechaAlta': alta,
+            'fechaInicio': ini,
+            'fechaTermino': ter,
+            'tipoUsuario': tu,
+            'user': user,
+            'password': pass,
+            'idArea': area
+        },
+        success: function (respuesta) {
+            alert(respuesta, data);
+            //mostrarDatosU("");
+        }
+    });
+}
+
+function validarTipeoCajas() {
+    //Validaciones de tipeo de teclas
+    $("#txtnombre").keypress(function (e) {
+        if (/[(A-Za-z)\s(áéíóú�?É�?ÓÚ)\.]/.test(String.fromCharCode(e.which)) == false) {
+            event.preventDefault();
+        }
+    });
+    $("#txtap").keypress(function (e) {
+        if (/[(A-Za-z)\s(áéíóú�?É�?ÓÚ)\.]/.test(String.fromCharCode(e.which)) == false) {
+            event.preventDefault();
+        }
+    });
+    $("#txtam").keypress(function (e) {
+        if (/[(A-Za-z)\s(áéíóú�?É�?ÓÚ)\.]/.test(String.fromCharCode(e.which)) == false) {
+            event.preventDefault();
+        }
+    });
+    $("#txtmatricula").keypress(function (e) {
+        if (/[(A-Za-z)(0-9)(áéíóú�?É�?ÓÚ)-]/.test(String.fromCharCode(e.which)) == false) {
+            event.preventDefault();
+        }
+    });
+    $("#txtcorreo").keypress(function (e) {
+        if (/[(A-Za-z)(0-9)@_-\.]/.test(String.fromCharCode(e.which)) == false){
+        event.preventDefault();
+    }
+    });
+    $("#txtinstitucion").keypress(function (e) {
+        if (/[(A-Za-z)(0-9)\s(áéíóú�?É�?ÓÚ)\.]/.test(String.fromCharCode(e.which)) == false) {
+            event.preventDefault();
+        }
+    });
+    $("#txtsemestre").keypress(function (e) {
+        if (/[(0-9)]/.test(String.fromCharCode(e.which)) == false) {
+            event.preventDefault();
+        }
+    });
+    $("#txttelefono").keypress(function (e) {
+        if (/[(0-9)]/.test(String.fromCharCode(e.which)) == false || $("#txttelefono").val().length >=  10) {            
+            event.preventDefault();
+        }
+    });
+    $("#txtuser").keypress(function (e) {
+        if (/[(A-Za-z)(0-9)@_-]/.test(String.fromCharCode(e.which)) == false) {
+            event.preventDefault();
+        }
+    });
+    $("#txtuser").keypress(function (e) {
+        if (/[(A-Za-z)(0-9)@_-\.]/.test(String.fromCharCode(e.which)) == false){
+        event.preventDefault();
+    }
+    });
 }
 
 
